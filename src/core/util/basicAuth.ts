49aa60cde1148df48764c04e5997f40967cb23d0
unknown <hidayatus_sholikhin_27rpl@student.smktelkom-mlg.sch.id>
2020-09-08 22:23:00 +0700

    Refactoring

--- src/core/util/basicAuth.ts
+++ src/core/util/basicAuth.ts
@@ -6,7 +6,7 @@ export default async function basicAuth (
   next: express.NextFunction) {
 
   if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
-    return res.status(401).json({ 'message': 'Anauthorized', 'status': 401 })
+    return res.status(401).json({ 'message': 'Unauthorized', 'status': 401 })
   }
   const base64Credentials = req.headers.authorization.split(' ')[1];
   const credentials = Buffer.from(base64Credentials, 'base64').toString();
